238772040c64cad123d50bd8f2d0732a7a06252e
Arthur Chaves <arthur.chsa@outlook.com>
2024-07-21 18:38:46 +0200

    Add service permissions for orchestrator

--- service_permissions/main.tf
+++ service_permissions/main.tf
@@ -1,6 +1,27 @@
-resource "google_project_iam_member" "service_agent_role" {
+resource "google_project_iam_member" "composer_1" {
   provider = google-beta
   project  = var.project_id
   member   = "serviceAccount:service-${var.project_number}@cloudcomposer-accounts.iam.gserviceaccount.com"
   role     = "roles/composer.ServiceAgentV2Ext"
+}
+
+resource "google_project_iam_member" "composer_2" {
+  provider = google-beta
+  project  = var.project_id
+  member   = "serviceAccount:service-${var.project_number}@cloudcomposer-accounts.iam.gserviceaccount.com"
+  role     = "roles/composer.serviceAgent"
+}
+
+resource "google_project_iam_member" "compute_1" {
+  provider = google-beta
+  project  = var.project_id
+  member   = "serviceAccount:service-${var.project_number}@compute-system.iam.gserviceaccount.com"
+  role     = "roles/compute.serviceAgent"
+}
+
+resource "google_project_iam_member" "container_1" {
+  provider = google-beta
+  project  = var.project_id
+  member   = "serviceAccount:service-${var.project_number}@container-engine-robot.iam.gserviceaccount.com"
+  role     = "roles/container.serviceAgent"
 }
